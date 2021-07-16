7566efd78c5dcb2171d168144b029270475528c6
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-16 22:58:27 +0300

    Second commit

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react';
 
 class Hello extends Component{
+
+    state = {
+        age:23,
+        user: {
+            name: "hussein",
+            lastName: "shokr"
+        },
+        socialLinks: [
+            "facebook",
+            "twitter",
+            "instagram"
+        ]
+    }
     render(){
-        return <div>Hello /\{this.props.name}/\ from class component.</div>
+        return (
+            <div>
+                Hello /\
+                {this.props.name}
+                /\ from class component. ||-- (props [name])
+
+                <p>Name: {this.state.user.name} | Age: {this.state.age} ||-- (State [name & age])</p>
+            </div>
+        )
     }
 }
 
