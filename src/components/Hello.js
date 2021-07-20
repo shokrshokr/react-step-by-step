9ce477ce3e0e96a0d748d35dc0acbdf74acb8b27
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-20 11:55:57 +0300

    005 commit

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -37,26 +37,52 @@ class Hello extends Component{
         this.setState({age: this.state.age - 1});
     }
 /**End // method four */
+/** Event onChange-updateName */
+    updateName(event){
+        //console.log(event.target.value)
+        const newName = event.target.value;
+        this.setState({
+            /** method one *//*
+                user:{
+                    name: newName,
+                    lastName: this.state.user.lastName
+                }
+            *//** End // method one */
+            /** method tow */
+            //bye5od lstate user metel ma heye byerja3 bi 8ayer li badna n8ayro
+                user:{
+                    ...this.state.user,
+                    name: newName
+                }
+            /** End // method tow */
+        });
+    }
+/** End // Event onChange-updateName */
     render(){
         return (
             <div>
-                Hello /\
-                {this.props.name}
-                /\ from class component. ||-- (props [name])
-
-                <p>Name: {this.state.user.name} | Age: {this.state.age} ||-- (State [name & age])</p>
-            {/**method one */}
+                {/*
+                    Hello /\
+                    {this.props.name}
+                    /\ from class component. ||-- (props [name])
+                */}
+                <p>Name: {this.state.user.name} | Age: {this.state.age} | lastName: {this.state.user.lastName} ||-- (State [name & age])</p>
+            {/**method one /}
                 <button onClick={()=>{this.setState({age: this.state.age + 1})}}>Increase</button>
-            {/** End // method one */}
-            {/**method tow */}
+            {/** End // method one /}
+            {/**method tow /}
                 <button onClick={this.decrement}>Decrease</button>
-            {/**End // method two */}
-            {/**method there */}
+            {/**End // method two /}
+            {/**method there /}
                 <button onClick={this.increase.bind(this)}>Increase with function</button>
-            {/**End // method there */}
-            {/**method four */}
+            {/**End // method there /}
+            {/**method four /}
                 <button onClick={this.dec}>dec with function</button>
-            {/**End // method four */}
+            {/**End // method four /}
+            <br />
+            {/** Event onChange */}
+                <input type="text" value={this.state.user.name} onChange={this.updateName.bind(this)} />
+            {/** End // Event onChange */}
             </div>
         )
     }
