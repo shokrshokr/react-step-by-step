de41bfbbe152bcffa01965be597b67fbb3bb2aef
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-20 11:29:56 +0300

    First commit

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 
 class Hello extends Component{
 
+    /*method four*/
+        constructor(props){
+            super(props);
+
+            this.dec = this.increase.bind(this);
+        }
+    /*End // method four*/
+
     state = {
         age:23,
         user: {
@@ -14,6 +22,21 @@ class Hello extends Component{
             "instagram"
         ]
     }
+/**method tow */
+    decrement = () => {
+        this.setState({age: this.state.age - 1});
+    }
+/**End // method tow */
+/**method there */
+    increase(){
+        this.setState({age: this.state.age + 1});
+    }
+/**End // method there */
+/**method four */
+    dec(){
+        this.setState({age: this.state.age - 1});
+    }
+/**End // method four */
     render(){
         return (
             <div>
@@ -22,6 +45,18 @@ class Hello extends Component{
                 /\ from class component. ||-- (props [name])
 
                 <p>Name: {this.state.user.name} | Age: {this.state.age} ||-- (State [name & age])</p>
+            {/**method one */}
+                <button onClick={()=>{this.setState({age: this.state.age + 1})}}>Increase</button>
+            {/** End // method one */}
+            {/**method tow */}
+                <button onClick={this.decrement}>Decrease</button>
+            {/**End // method two */}
+            {/**method there */}
+                <button onClick={this.increase.bind(this)}>Increase with function</button>
+            {/**End // method there */}
+            {/**method four */}
+                <button onClick={this.dec}>dec with function</button>
+            {/**End // method four */}
             </div>
         )
     }
