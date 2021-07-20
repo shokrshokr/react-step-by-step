2a377069f290decdcd42f08f2bc41e1cab548e21
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-20 21:27:52 +0300

    011-part2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import AgeComponent from './components/Age';
+import UsersList from './components/UsersList';
 
 class App extends Component{
 
 
   render(){
-
     const age= 10;
-    let ageTemplate = '';
-    let users = [
-      {id: 1, name: "hussein", age: 23},
-      {id: 2, name: "Mohamad", age: 25},
-      {id: 3, name: "Ali", age: 27},
-    ]
+    /* loop - list */
+      let users = [
+        {id: 1, name: "hussein", age: 23},
+        {id: 2, name: "Mohamad", age: 25},
+        {id: 3, name: "Ali", age: 27},
+      ]
 
-    if(age>5){
-      ageTemplate = <h1>you are older than 5</h1>;
-    } else{
-      ageTemplate = <p>you are not older than 5</p>;
-    }
-
+      let guests = [
+        {id: 1, name: "huss", age: 2},
+        {id: 2, name: "Moha", age: 5},
+      ]
+    /* End // loop - list */
     return(
       <div className="App">
-        {ageTemplate}
-        <ul>
-          {users.map((user, index) => <li key={user.id}>{index} - {user.name}</li>)}
-        </ul>
+        <AgeComponent age={age} />
+        <AgeComponent age={3} />
+
+        <UsersList users={users} />
+        <UsersList users={guests} />
         
       </div>
     );
