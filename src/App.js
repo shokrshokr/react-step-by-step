94b263d7665b7a49199f9ca2db4150ae93148e0a
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-22 21:45:19 +0300

    015-part3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 class App extends Component{
   
   state = {
-    value: 'Default'
+    value: 'Default',
+    selectVal: "2"
   }
   
   setValue = (event) => {
@@ -16,15 +17,24 @@ class App extends Component{
     })
   }
 
+  setSelectValue= (event) => {
+    this.setState({
+      selectVal: event.target.value
+    })
+  }
+
 
   render(){
     return(
       <div className="App">
-        
-
         <input value={this.state.value} onChange={this.setValue} />
         <hr />
         <textarea value={this.state.value} onChange={this.setValue} />
+        <hr />
+        <select value={this.state.selectVal} onChange={this.setSelectValue} >
+          <option value="1">One</option>
+          <option value="2">Two</option>
+        </select>
       </div>
     );
   }
