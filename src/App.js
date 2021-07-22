9152e13ab948090a813b9a3ef599adfd58c1d57d
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-07-22 21:31:29 +0300

    013-part3

--- src/App.js
+++ src/App.js
@@ -1,79 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Task from './components/Task';
-import CreateTask from './components/CreateTask';
 
 
 
 class App extends Component{
+  
   state = {
-    newTask : "",
-    todos: [
-      {text: 'learn ReactJs'},
-      {text: 'learn AngularJs'},
-      {text: 'learn NodeJs'},
-      {text: 'learn VueJs'},
-    ]
+    value: 'Default'
   }
-  completeTask = (index) => {
-    //console.log(index);
-    const todos = [...this.state.todos];
-    todos.splice(index, 1);
+  
+  setValue = (event) => {
     this.setState({
-      todos
-    });
-  }
-
-  updateNewTask = (event) => {
-    this.setState({
-      newTask: event.target.value
-    });
-  }
-
-  addTask = () => {
-    const todos = [...this.state.todos];
-    todos.push({
-      text: this.state.newTask
-    });
-    this.setState({
-      todos,
-      newTask: ''
-    });
+      value: event.target.value
+    })
   }
 
 
   render(){
     return(
       <div className="App">
-        {/** Method one */}
-          {/*this.state.todos.map((todo, index) => 
-            <Task 
-              todo={todo} 
-              completeTask={this.completeTask} 
-              index={index} 
-              key={index} 
-            /> )
-          */}
-        {/** End // Method one */}
-        {/** Method two */}
-          {this.state.todos.map((todo, index) => 
-            <Task 
-              todo={todo} 
-              completeTask={() => 
-                this.completeTask(index)
-              } 
-              key={index} 
-            /> 
-          )}
-        {/** End // Method two */}
-
-        <CreateTask 
-          value={this.state.newTask} 
-          onChange={this.updateNewTask} 
-          addTask={this.addTask} 
-        />
         
+
+        <input value={this.state.value} onChange={this.setValue} />
       </div>
     );
   }
