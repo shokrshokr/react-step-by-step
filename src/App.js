c0099ef054b13d1b8e659faa89e8fec75ffdb4bd
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-10 12:47:57 +0300

    026-part5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import './App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    className: 'green'
+  }
   render() {
+    const className = 'green';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className='green'>
+          Test
+        </div>
+        <br />
+        <div className={className}>
+          Test Two
+        </div>
+        <br />
+        <p>men 5elel state</p>
+        <div className={this.state.className}>
+          Test Three
+        </div>
+
+        <button onClick={()=> {
+          this.setState({className: 'red'})
+        }}>
+          Change to red
+        </button>
+        <button onClick={()=> {
+          this.setState({className: 'green'})
+        }}>
+          Change to green
+        </button>
       </div>
     );
   }
