169c4609b69b8a4eae0bd9765991fea43d3090ac
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-14 17:47:30 +0300

    033-part6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import ViewUser from './components/ViewUser';
 import { getUsers, deleteUser } from './Api/Users';
+import UsersForm  from './components/UsersForm'
 
 class App extends Component {
 
@@ -27,14 +28,6 @@ class App extends Component {
   }
 
   deleteUser = (user) => {
-    // delete from state
-    /*
-    let users = this.state.users;
-    const index = users.indexOf(user);
-    users.splice(index, 1);
-    this.setState({users});
-    */
-
     //delete from server
     deleteUser(user.id)
       .then(() => {
@@ -65,6 +58,18 @@ class App extends Component {
           <h3>User Details</h3>
           {this.state.user.id > 0 ? 
             <ViewUser user={this.state.user} / >
+            :
+            'please select a user'
+          }
+        </div>
+
+        <div>
+          <h3>Edit User</h3>
+          {this.state.user.id > 0 ? 
+            <UsersForm 
+              values={this.state.user} 
+              onSubmit={(values => console.log(values))}  
+            />
             :
             'please select a user'
           }
