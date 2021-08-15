572c8a167497f31376fea889f9b49e01191e8fda
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-15 13:43:55 +0300

    037-part7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from "react-router-dom";
+import UserPage from './Pages/UserPage';
+import UsersPage from './Pages/UsersPage';
 
 
 const HomePage = () => <div>Home</div>
@@ -11,9 +13,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Link to="/">Home</Link>{" "}
+          <Link to="/users">Users</Link>{" "}
           <Link to="/about">About</Link>
 
           <Route path="/" exact component={HomePage} />
+          <Route path="/users" exact component={UsersPage} />
+          <Route path="/users/:id" exact component={UserPage} />
           <Route path="/about" component={AboutPage} />
         
         </div>
