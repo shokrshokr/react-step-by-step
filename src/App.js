61143e5475823db444947da527d338e48a78c538
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-15 14:41:02 +0300

    040-part7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Link, Switch} from "react-router-dom";
+import {BrowserRouter, Route, NavLink, Switch} from "react-router-dom";
 import CreateUserPage from './Pages/CreateUserPage';
 import EditUserPage from './Pages/EditUserPage';
 import UserPage from './Pages/UserPage';
 import UsersPage from './Pages/UsersPage';
-
+import "./App.css";
 
 const HomePage = () => <div>Home</div>
 const AboutPage = () => <div>About</div>
+
+const MyNavLink = (props) => <NavLink exact activeClassName="active" {...props}>{props.children}</NavLink>
+
 class App extends Component {
   
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Link to="/">Home</Link>{" "}
-          <Link to="/users">Users</Link>{" "}
-          <Link to="/users/create">Create User</Link>{" "}
-          <Link to="/about">About</Link>
+          <MyNavLink to="/">Home</MyNavLink>{" "}
+          <MyNavLink to="/users">Users</MyNavLink>{" "}
+          <MyNavLink to="/users/create">CreateUser</MyNavLink>{" "}
+          <MyNavLink to="/about">About</MyNavLink>
           <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/about" component={AboutPage} />
