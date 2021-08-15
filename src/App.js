da3abf99b62de3f89dbb3ef0433cff3b64318092
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-15 14:28:27 +0300

    039-part7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Link} from "react-router-dom";
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom";
+import CreateUserPage from './Pages/CreateUserPage';
 import EditUserPage from './Pages/EditUserPage';
 import UserPage from './Pages/UserPage';
 import UsersPage from './Pages/UsersPage';
@@ -15,13 +16,18 @@ class App extends Component {
         <div className="App">
           <Link to="/">Home</Link>{" "}
           <Link to="/users">Users</Link>{" "}
+          <Link to="/users/create">Create User</Link>{" "}
           <Link to="/about">About</Link>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/users" exact component={UsersPage} />
+            <Route path="/users/create" exact component={CreateUserPage} />
+            <Route path="/users/:id" exact component={UserPage} />
+            <Route path="/users/edit/:id" exact component={EditUserPage} />
 
-          <Route path="/" exact component={HomePage} />
-          <Route path="/users" exact component={UsersPage} />
-          <Route path="/users/:id" exact component={UserPage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/users/edit/:id" exact component={EditUserPage} />
+          </Switch>
+          
         
         </div>
       </BrowserRouter>
