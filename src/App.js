39f5a4a178ff5d5d0e4948eac4798cfe6d688e4e
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-16 17:41:02 +0300

    046-part8-Redux

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Cart from './pages/Cart';
+import Products from './pages/Products';
+
+class App extends Component{
+  
+
+
+  render(){
+    return(
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">CatsStore</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/products">Products</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/cart" className="nav-link">Cart</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+
+          <Route path="/" exact component={Home} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/products" component={Products} />
+
+
+
+        </div>
+      </Router>
+        
+      
+    );
+  }
+}
+
+export default App;
