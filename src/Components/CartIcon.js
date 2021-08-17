b8dace73476a3b1033e8d82e6c957a6160cb3779
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-17 12:22:25 +0300

    052-part8-Redux

--- src/Components/CartIcon.js
+++ src/Components/CartIcon.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./CartIcon.css";
 
 function CartIcon(Props){
     return (
         <div id="cart-icon">
+            <Link to="/cart">
                 <i className="fa fa-shopping-cart"></i>
                 <span className="badge badge-danger">2</span>
+            </Link>
         </div>
     )
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import CartItem from "../Components/CartItem";
+import ProductsApi from "../api/products"
 
-function Cart() {
-  return (
-    <div>
-        Cart
-    </div>
-  )
+class Cart extends React.Component{
+
+    state = {
+      products: [],
+    }
+
+    componentDidMount(){
+      ProductsApi.getAll()
+        .then(data => {
+          this.setState({
+            products: data
+          })
+        })
+    }
+    
+
+    render(){
+        return (
+            <div>
+                <h1>Cart</h1>
+
+                <div className="row">
+                    {this.state.products.map(product => 
+                        <div className={'col-3'} key={product.id}>
+                            <CartItem product={product} />
+                        </div>
+                    )}
+                </div>
+
+                <br />
+                <h3>
+                    Total: 3000
+                </h3>
+
+                <button className="btn btn-primary btn-block">Pay</button>
+            </div>
+        );
+    }
 }
 
-export default Cart
+
+
+
+export default Cart;
