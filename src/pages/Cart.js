5c121edd36d8c6ea6827257643cea4d2dd05a457
shokr6252511312@gmail.com <shokr6252511312@gmail.com>
2021-08-17 13:22:20 +0300

    054-part8-Redux

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,22 +1,16 @@
 import React from "react";
 import CartItem from "../Components/CartItem";
-import ProductsApi from "../api/products"
+import ProductsApi from "../api/products";
+import {connect} from "react-redux";
 
 class Cart extends React.Component{
 
-    state = {
-      products: [],
-    }
-
-    componentDidMount(){
-      ProductsApi.getAll()
-        .then(data => {
-          this.setState({
-            products: data
-          })
-        })
-    }
-    
+    placeOrder = () => {
+        // send the request to the server
+        // clear cart
+        this.props.clearCart();
+        alert('We recieved your order, and we are working on it.');
+    };
 
     render(){
         return (
@@ -24,25 +18,31 @@ class Cart extends React.Component{
                 <h1>Cart</h1>
 
                 <div className="row">
-                    {this.state.products.map(product => 
-                        <div className={'col-3'} key={product.id}>
-                            <CartItem product={product} />
+                    {this.props.cartItems.map(item => 
+                        <div className={'col-3'} key={item.product.id}>
+                            <CartItem item={item} />
                         </div>
                     )}
                 </div>
 
                 <br />
                 <h3>
-                    Total: 3000
+                    Total: {this.props.total}
                 </h3>
 
-                <button className="btn btn-primary btn-block">Pay</button>
+                <button className="btn btn-primary btn-block" onClick={this.placeOrder}>Place order</button>
             </div>
         );
     }
 }
 
 
+const mapStateToProps = (state) => {
+    return {
+        cartItems: state.cart,
+        total: state.cart.reduce((total, item) => total + item.quantity * item.product.price, 0),
+    };
+}
 
 
-export default Cart;
+export default connect(mapStateToProps)(Cart);
